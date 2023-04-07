559ee16d12b0976ffbdb09370f8c4bd021eb2ddc
KamilS31 <sadowskiagh@student.agh.edu.pl>
2023-04-07 15:25:54 +0200

    objective_function_added

--- GA_TSP.m
+++ GA_TSP.m
@@ -1,8 +1,17 @@
 close all; clc; clear;
 load('usborder.mat','x','y','xx','yy');
-nCities = 20;  % O(N^2)
+nCities = 50;  % O(N^2)
 citiesLat = zeros(nCities,1);
 citiesLon = citiesLat;
+order = 1:nCities;
+nPopulation = 20; % population size
+
+% generating an initial population
+chromosome = zeros(nPopulation, nCities);
+for i = 1:nPopulation
+    perm = randperm(length(order));
+    chromosome(i,:) = order(perm);
+end
 
 n = 1;
 while (n <= nCities)
@@ -18,14 +27,24 @@ plot(214*x,300*y)
 hold on
 scatter(citiesLat,citiesLon,'Filled')
 
-order = 1:nCities;
-
-dist = objective_function(citiesLat,citiesLon,order);
+[dist] = objective_function(citiesLat, citiesLon, chromosome, nPopulation, nCities);
+[list, index] = reproduction_probability(dist, chromosome);
 
 % Function definitions:
-function dist = objective_function(citiesLat, citiesLon,order)
-    dist = sqrt((citiesLat(order(2:end)) - citiesLat(order(1:end-1))).^2 + (citiesLon(order(2:end)) - citiesLon(order(1:end-1))).^2);
-    A = sqrt((citiesLat(order(1)) - citiesLat(order(end))).^2 + (citiesLon(order(1)) - citiesLon(order(end))).^2);
-    dist = sum([dist', A]);
+function [dist] = objective_function(citiesLat, citiesLon, chromosome, nPopulation, nCities)
+    dist_mx = zeros(nPopulation, nCities);
+    for i = 1:nPopulation
+        dist_mx(i,1:end-1) = sqrt((citiesLat(chromosome(i,2:end)) - citiesLat(chromosome(i,1:end-1))).^2 + (citiesLon(chromosome(i,2:end)) - citiesLon(chromosome(i,1:end-1))).^2);
+        dist_mx(i,end) = sqrt((citiesLat(chromosome(i,1)) - citiesLat(chromosome(i,end))).^2 + (citiesLon(chromosome(i,1)) - citiesLon(chromosome(i,end))).^2);
+    end 
+    dist = sum(dist_mx, 2);
 end
+
+function [list, index] = reproduction_probability(dist, chromosome)
+    list = [dist, chromosome];
+    [list, index] = sortrows(list,1);
+    list = [index, list, [1:length(dist)]'];
+end
+
+
 
