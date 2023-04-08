b039f3fe31c8ce6f7168642b8c032de3a4066f9c
KamilS31 <sadowskiagh@student.agh.edu.pl>
2023-04-09 00:03:27 +0200

    crossover added

--- GA_TSP.m
+++ GA_TSP.m
@@ -1,6 +1,6 @@
 close all; clc; clear;
 load('usborder.mat','x','y','xx','yy');
-nCities = 50;  % O(N^2)
+nCities = 5;  % O(N^2)
 citiesLat = zeros(nCities,1);
 citiesLon = citiesLat;
 order = 1:nCities;
@@ -31,10 +31,11 @@ scatter(citiesLat,citiesLon,'Filled')
 [probability] = selection_probability(dist, nPopulation, chromosome);
 
 mate = [];
-for i = 1:5
+for i = 1:nPopulation
     [mate] = [mate; randsample(1:nPopulation, 2, true, probability(:,1))];
 end
 
+[cros,y,z] = crossover(mate, probability, nCities);
 
 % Function definitions:
 function [dist] = objective_function(citiesLat, citiesLon, chromosome, nPopulation, nCities)
@@ -52,6 +53,37 @@ function [probability] = selection_probability(dist, nPopulation, chromosome)
     rank_sum = nPopulation .* (nPopulation + 1) ./ 2;
     probability = (1:nPopulation) / rank_sum;
     probability = [probability', list];
+end
+
+function [cros,y,z] = crossover(mate, probability, nCities)
+    [cros] = [probability(mate(1,1),3:end); probability(mate(1,2),3:end)];
+    start = randi(nCities); % random start point
+    dl = randi([0,nCities - start]); % random number of indexes to take, 0 or sth.
+    for i = start:start+dl
+        temp = cros(1,i); % value of first vector
+        cros(1,i) = cros(2,i);
+        cros(2,i) = temp;
+    end
+    disp(start)
+    disp(dl)
+    disp('----------')
+    T1 = [cros(1,1:start-1), zeros(1,length(start:start+dl)), cros(1,start+dl+1:end)];
+    y = [];
+    for j = start:start+dl
+        [y] = [y, find(T1(1,:) == cros(1,j))];
+    end
+    szukana = start:start+dl;
+    temp = cros(2, szukana);
+    cros(2, szukana) = 0;
+    z = [];
+    for k = temp
+        [z] = [z, find(cros(2,:) == k)];
+    end
+    cros(2, szukana) = temp;
+
+    temp = cros(1,y); 
+    cros(1,y) = cros(2,z);
+    cros(2,z) = temp;
 end
 
 
